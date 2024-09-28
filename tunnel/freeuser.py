4645f621b779945eb8513c2117a974e57f6a8887
Bagus Vendi Vannessa <vorstcavry@gmail.com>
2024-09-28 14:59:31 +0700

    Update and rename freeuser.py to tunnel/freeuser.py

--- tunnel/freeuser.py
+++ tunnel/freeuser.py
@@ -17,7 +17,7 @@ from datetime import timedelta
 def detect_environment():
     free_plan = (os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES') / (1024. ** 3) <= 20)
     environments = {
-        'COLAB_GPU': ('Google Colab', "/root" if free_plan else "/content"),
+        'COLAB_GPU': ('Google Colab', "/content" if free_plan else "/content"),
         'KAGGLE_URL_BASE': ('Kaggle', "/kaggle/working/content")
     }
 
@@ -26,7 +26,7 @@ def detect_environment():
             return environment, path, free_plan
 
 env, root_path, free_plan = detect_environment()
-webui_path = f"{root_path}/vorst-cavry"
+webui_path = f"{root_path}/vorstcavry"
 
 commandline_arguments = "--listen --enable-insecure-extension-access --theme dark --no-half-vae --disable-console-progressbars --no-hashing --opt-sdp-attention" #@param{type:"string"}
 # ======================== TUNNEL ========================
